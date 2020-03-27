a893a9b014a8518460619f5769620741cb7f288d
wojtekMroczkowski <wojtek.mroczkowski@gmail.com>
2020-03-27 10:52:22 +0100

    Add facereco app frontend

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './FaceRecognition.css';
+
+const FaceRecognition = ({box, imageUrl}) => {
+    return (
+       <div className='center'>
+       <div className='absolute mt2' >
+           <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto' />
+           { box.topRow ? <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div> : '' }
+        </div>
+       </div>
+    );
+}
+
+export default FaceRecognition;
